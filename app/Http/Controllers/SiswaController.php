be82983f9f1c5c15f1ce3baa85f83d8afc1d8f99
fahrihadi1 <syamsulfahrihadi@live.com>
2019-05-27 09:50:58 +0700

    coba model

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiswaController extends Controller
+{
+    public function index()
+    {
+        $data_siswa = \App\Siswa::all();
+        return view('siswa.index',compact('data_siswa'));
+    }
+}
